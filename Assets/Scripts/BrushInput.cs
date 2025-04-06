1d94a250870440df70680495945383ef5f8573dd
NeoThePotato <neorayb@gmail.com>
2025-04-06 14:16:05 +0300

    Let BrushInput control brush scale.

--- Assets/Scripts/BrushInput.cs
+++ Assets/Scripts/BrushInput.cs
@@ -7,12 +7,16 @@ using Unity.Mathematics;
 public class BrushInput : MonoBehaviour
 {
 	[SerializeField] private BrushController _brushController;
+	[SerializeField] private float _scrollSensitivity = 2e-4f;
+	[SerializeField] private float _minSize = 0.02f, _maxSize = 1f;
 
 	private ref BrushController.BrushData Brush => ref _brushController.Brush;
 
 	private static ButtonControl MainButton => Mouse.current.leftButton;
 
 	private static ButtonControl SecondaryButton => Mouse.current.rightButton;
+
+	private static DeltaControl ScaleInput => Mouse.current.scroll;
 
 	private void OnEnable()
 	{
@@ -37,6 +41,8 @@ public class BrushInput : MonoBehaviour
 			Brush.UV = hit.textureCoord;
 			Brush.Intensity = math.abs(Brush.Intensity) * intensity;
 		}
+		if (ScaleInput.IsActuated())
+			Brush.Scale = math.clamp(Brush.Scale + ScaleInput.ReadValue().y * _scrollSensitivity, _minSize, _maxSize);
 
 		static bool PaintActive(out RaycastHit hit, out float intensity)
 		{
