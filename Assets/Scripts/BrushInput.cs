03919669b79c34e2254d3bccbea8c19700005bb6
NeoThePotato <neorayb@gmail.com>
2025-04-12 14:57:34 +0300

    Bind sliders to brush input.

--- Assets/Scripts/BrushInput.cs
+++ Assets/Scripts/BrushInput.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
+using UnityEngine.UI;
 using Unity.Burst;
 using Unity.Mathematics;
 
@@ -9,6 +10,7 @@ public class BrushInput : MonoBehaviour
 	[SerializeField] private BrushController _brushController;
 	[SerializeField] private float _scrollSensitivity = 2e-4f;
 	[SerializeField] private float _minSize = 0.02f, _maxSize = 1f;
+	[SerializeField] private Slider _intensitySlider, _scaleSlider;
 
 	private ref BrushController.BrushData Brush => ref _brushController.Brush;
 
@@ -21,26 +23,44 @@ public class BrushInput : MonoBehaviour
 	private void Start()
 	{
 		var ui = UIManager.Instance;
-		Brush.Color = ui.CurrentlySelectedColor;
-		Brush.texture = ui.CurrentlySelectedTexture;
+		Brush.Color = ui.DefaultColor;
+		Brush.texture = ui.DefaultTexture;
+		if (_scaleSlider)
+		{
+			_scaleSlider.minValue = _minSize;
+			_scaleSlider.maxValue = _maxSize;
+			_scaleSlider.value = Brush.Scale;
+		}
+		if (_intensitySlider)
+			_intensitySlider.value = Brush.Intensity;
 	}
 
 	private void OnEnable()
 	{
-		if (!UIManager.Instance)
+		var ui = UIManager.Instance;
+		if (!ui)
 			return;
-		UIManager.Instance.OnPaintBrushSelected += OnPaintBrushSelected;
-		UIManager.Instance.OnVertexBrushSelected += OnVertexBrushSelected;
-		UIManager.Instance.OnTextureChanged += OnTextureChanged;
+		ui.OnPaintBrushSelected += OnPaintBrushSelected;
+		ui.OnVertexBrushSelected += OnVertexBrushSelected;
+		ui.OnTextureChanged += OnTextureChanged;
+		if (_intensitySlider)
+			_intensitySlider.onValueChanged.AddListener(OnIntensityChanged);
+		if (_scaleSlider)
+			_scaleSlider.onValueChanged.AddListener(OnSizeChanged);
 	}
 
 	private void OnDisable()
 	{
-		if (!UIManager.Instance)
+		var ui = UIManager.Instance;
+		if (!ui)
 			return;
-		UIManager.Instance.OnPaintBrushSelected -= OnPaintBrushSelected;
-		UIManager.Instance.OnVertexBrushSelected -= OnVertexBrushSelected;
-		UIManager.Instance.OnTextureChanged -= OnTextureChanged;
+		ui.OnPaintBrushSelected -= OnPaintBrushSelected;
+		ui.OnVertexBrushSelected -= OnVertexBrushSelected;
+		ui.OnTextureChanged -= OnTextureChanged;
+		if (_intensitySlider)
+			_intensitySlider.onValueChanged.RemoveListener(OnIntensityChanged);
+		if (_scaleSlider)
+			_scaleSlider.onValueChanged.RemoveListener(OnSizeChanged);
 	}
 
 	private void Update()
@@ -48,10 +68,15 @@ public class BrushInput : MonoBehaviour
 		if (Brush.PaintingMode = PaintActive(out var hit, out var intensity))
 		{
 			Brush.UV = hit.textureCoord;
-			Brush.Intensity = math.abs(Brush.Intensity) * intensity;
+			var absIntensity = math.abs(Brush.Intensity);
+			Brush.Intensity = absIntensity * intensity;
 		}
 		if (ScaleInput.IsActuated())
+		{
 			Brush.Scale = math.clamp(Brush.Scale + ScaleInput.ReadValue().y * _scrollSensitivity, _minSize, _maxSize);
+			if (_scaleSlider)
+				_scaleSlider.value = Brush.Scale;
+		}
 
 		static bool PaintActive(out RaycastHit hit, out float intensity)
 		{
@@ -83,4 +108,8 @@ public class BrushInput : MonoBehaviour
 	}
 
 	private void OnTextureChanged(Texture texture) => Brush.texture = texture;
+
+	private void OnSizeChanged(float size) => Brush.Scale = size;
+
+	private void OnIntensityChanged(float intensity) => Brush.Intensity = intensity;
 }
